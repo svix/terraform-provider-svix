7115996c9fff260e480818025a5286012ee606d6
Mendy Man <mman@svix.com>
2025-07-09 10:08:01 -0400

    Release `v0.3.0`

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     svix = {
       source  = "registry.terraform.io/svix/svix"
-      version = "0.2.4"
+      version = "0.3.0"
     }
   }
 }
--- internal/version.go
+++ internal/version.go
@@ -1,3 +1,3 @@
 package internal
 
-var Version = "0.2.4"
+var Version = "0.3.0"
